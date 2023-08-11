e4f0346e2e9ac2dffd475b8a883b3e2084710631
Abdullah Elsayed <11673834+algorizr@users.noreply.github.com>
2023-08-11 21:02:07 +0100

    feat: create auto layouting for flows

--- src/components/flow-graph.tsx
+++ src/components/flow-graph.tsx
@@ -1,87 +1,29 @@
-import { useCallback, useEffect } from "react";
+"use client";
+
 import ReactFlow, {
   Background,
   BackgroundVariant,
-  Edge,
   MiniMap,
-  Node,
   NodeTypes,
-  useEdgesState,
-  useNodesState,
 } from "reactflow";
 import { type ModuleItem } from "@swc/wasm-web";
+
+import useFlow from "@/hooks/useFlow";
 
 import FunctionFlowNode from "./function-flow-node";
 
 import "reactflow/dist/style.css";
 
-const nodeTypes: NodeTypes = {
-  functionFlowNode: FunctionFlowNode,
-};
-
-function getNodesAndEdges(
-  nodes: Node[],
-  edges: Edge[],
-  ast: ModuleItem[],
-  currPos: { x: number; y: number },
-  source?: string
-) {
-  ast.map((item) => {
-    switch (item.type) {
-      case "FunctionDeclaration":
-        if (source)
-          edges.push({
-            id: `${source}-${item.span.start}-${item.span.end}`,
-            source,
-            target: `${item.span.start}-${item.span.end}`,
-          });
-
-        nodes.push({
-          id: `${item.span.start}-${item.span.end}`,
-          data: { ...item },
-          type: "functionFlowNode",
-          position: currPos,
-        });
-
-        if (item.body?.stmts) {
-          const newPos = { x: currPos.x, y: currPos.y };
-          getNodesAndEdges(
-            nodes,
-            edges,
-            item.body.stmts,
-            newPos,
-            `${item.span.start}-${item.span.end}`
-          );
-        }
-        break;
-      default:
-        break;
-    }
-  });
-}
-
 interface Props {
   ast: ModuleItem[];
 }
 
+const nodeTypes: NodeTypes = {
+  functionFlowNode: FunctionFlowNode,
+};
+
 export default function FlowGraph({ ast }: Props) {
-  const [nodes, setNodes, onNodesChange] = useNodesState([]);
-  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
-
-  const handleChange = useCallback(
-    (ast: ModuleItem[]) => {
-      const newNodes: Node[] = [];
-      const newEdges: Edge[] = [];
-      getNodesAndEdges(newNodes, newEdges, ast, { x: 0, y: 0 });
-      setNodes([...newNodes]);
-      setEdges([...newEdges]);
-    },
-    [setEdges, setNodes]
-  );
-
-  useEffect(() => {
-    handleChange(ast);
-  }, [ast, handleChange]);
+  const { nodes, onNodesChange, edges, onEdgesChange } = useFlow(ast);
 
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
